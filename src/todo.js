15e655553a09d0fd3ae93fe959aa33ebcb12b7fa
Darek Greenly <git@zielak.pl>
2016-07-12 19:14:41 +0200

    first day done

--- src/todo.js
+++ src/todo.js
@@ -1,3 +1,4 @@
+
 var todoApp = angular.module('todoApp', []);
 
 todoApp.controller('todoController', function appController($scope){
@@ -20,9 +21,47 @@ todoApp.controller('todoController', function appController($scope){
     {title: 'make some dev', done: true},
   ];
 
+  $scope.defaultTasks = $scope.tasks.slice();
+
   $scope.reorderTasks = function(){
     $scope.tasks.sort(compareTasks);
-  }
+  };
+
+  $scope.checkForEmptyTask = function(){
+    for (var i = $scope.tasks.length - 1; i >= 0; i--) {
+      if($scope.tasks[i].title.length <= 0 && !$scope.tasks[i].done){
+        return i;
+      }
+    }
+    return -1;
+  };
+
+  $scope.addNewTask = function(){
+
+    var empty = $scope.checkForEmptyTask();
+    if(empty >= 0){
+      document.querySelectorAll(".task .text-input input")[empty].focus();
+    }else{
+      $scope.tasks.push({title: '', done: false});
+      $scope.reorderTasks();
+    }
+
+  };
+
+  $scope.removeTask = function(task) {
+    var idx = $scope.tasks.indexOf(task);
+    if(idx >= 0){
+      $scope.tasks.splice(idx, 1);
+    }
+  };
+
+  $scope.resetTasks = function(){
+    $scope.tasks = $scope.defaultTasks.slice();
+  };
+
+  $scope.clearTasks = function(){
+    $scope.tasks = [];
+  };
 
   $scope.taskStatusChange = function(task){
     console.log("Task status change", task);
